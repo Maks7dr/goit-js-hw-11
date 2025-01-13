ebbc466fae4a1b32b92a04a7a043f4a96f52aae3
Maksym Drachenko <maks7d@gmail.com>
2025-01-13 16:51:07 +0500

    add task

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,72 +1,22 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+const API_KEY = '48145276-ba1887f72c90a3fcdb02285cd';
 
-const form = document.querySelector('#formImg');
-const input = document.querySelector('#searchImg');
+export async function fetchImages(query) {
+  const option = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  if (input.value.trim() === '') {
-    iziToast.error({
-      title: 'Error',
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-    });
+  try {
+    const response = await fetch(`https://pixabay.com/api/?${option}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch images');
+    }
+    return response.json();
+  } catch (error) {
+    console.error(error.message);
+    throw error;
   }
-});
-
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-
-// const form = document.querySelector('#formImg');
-// const input = document.querySelector('#searchImg');
-
-// form.addEventListener('submit', async event => {
-//   event.preventDefault();
-
-//   const query = input.value.trim();
-
-//   if (query === '') {
-//     iziToast.error({
-//       title: 'Error',
-//       message:
-//         'Sorry, there are no images matching your search query. Please try again!',
-//     });
-//     return; // Прерываем выполнение кода
-//   }
-
-//   try {
-//     const response = await fetch(
-//       `https://pixabay.com/api/?key=YOUR_API_KEY&q=${encodeURIComponent(
-//         query
-//       )}&image_type=photo`
-//     );
-
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-
-//     const data = await response.json();
-
-//     if (data.hits.length === 0) {
-//       iziToast.error({
-//         title: 'Error',
-//         message:
-//           'No images found for your query. Please try another search term.',
-//       });
-//     } else {
-//       iziToast.success({
-//         title: 'Success',
-//         message: `Found ${data.hits.length} images!`,
-//       });
-//       // Добавьте отображение изображений здесь
-//       console.log(data.hits);
-//     }
-//   } catch (error) {
-//     iziToast.error({
-//       title: 'Error',
-//       message: 'An error occurred. Please try again later.',
-//     });
-//     console.error('Error:', error);
-//   }
-// });
+}
